c3e5740f3c696b6c0fa9daf3cb06aee2dd7928c5
daniishhhhh <daishsidiq500@gmail.com>
2026-01-25 14:49:42 +0530

    Fix API routing + add health endpoint

--- backend/chemical_backend/urls.py
+++ backend/chemical_backend/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def health(request):
+    return JsonResponse({"status": "Backend Running"})
+
 
 urlpatterns = [
+    path("", health),
     path("admin/", admin.site.urls),
     path("api/", include("equipment.urls")),
 ]
